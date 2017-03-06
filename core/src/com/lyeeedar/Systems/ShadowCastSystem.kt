e0091f681cdef9a693b13f2cd43d4f3f06c5e93d
infinity8 <lyeeedar@gmail.com>
2017-03-06 17:35:37 +0000

    Multithread shadowcasting

--- core/src/com/lyeeedar/Systems/ShadowCastSystem.kt
+++ core/src/com/lyeeedar/Systems/ShadowCastSystem.kt
@@ -10,23 +10,44 @@ import com.lyeeedar.Level.Level
 
 class ShadowCastSystem(): AbstractSystem(Family.all(PositionComponent::class.java).one(ShadowCastComponent::class.java, LightComponent::class.java).get())
 {
+	private val jobs = Array<Job>()
+	
 	override fun doUpdate(deltaTime: Float)
 	{
-		for (entity in entities)
-		{
-			val tile = entity?.tile() ?: continue
-
-			if (tile.taxiDist(level!!.player.tile()!!) > 100)
+		jobs.clear()
+		
+		runBlocking {
+			for (entity in entities)
 			{
-				continue
+				val tile = entity?.tile() ?: continue
+
+				if (tile.taxiDist(level!!.player.tile()!!) > 100)
+				{
+					continue
+				}
+
+				val stats = Mappers.stats.get(entity)
+				val shadow = Mappers.shadow.get(entity)
+				val light = Mappers.light.get(entity)
+
+				if (shadow != null && stats != null)
+				{
+					val job = launch(CommonPool) {
+						shadow.cache.getShadowCast(tile.level.grid, tile.x, tile.y, stats.sight.toInt(), entity)
+					}
+					jobs.add(job)
+				}
+				
+				if (light != null) 
+				{
+					val job = launch(CommonPool) {
+						light.cache.getShadowCast(tile.level.grid, tile.x, tile.y, Math.ceil(light.dist.toDouble()).toInt(), entity)
+					}
+					jobs.add(job)
+				}
 			}
 
-			val stats = Mappers.stats.get(entity)
-			val shadow = Mappers.shadow.get(entity)
-			val light = Mappers.light.get(entity)
-
-			if (shadow != null && stats != null) shadow.cache.getShadowCast(tile.level.grid, tile.x, tile.y, stats.sight.toInt(), entity)
-			if (light != null) light.cache.getShadowCast(tile.level.grid, tile.x, tile.y, Math.ceil(light.dist.toDouble()).toInt(), entity)
+			for (job in jobs) job.Join()
 		}
 	}
 }
